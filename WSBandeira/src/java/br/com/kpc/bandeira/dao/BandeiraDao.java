ffdc151b0e74a9e5e2c99d08c6cc1d1366de51ce
kennedimalheiros <kennedimalheiros@gmail.com>
2014-09-06 10:48:35 -0300

    Commit Inicial, ClassDao da Bandeira

---
+++ WSBandeira/src/java/br/com/kpc/bandeira/dao/BandeiraDao.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.kpc.bandeira.dao;
+
+import br.com.kpc.bandeira.core.Bandeira;
+import br.com.kpc.bandeira.core.IRepositoryBandeira;
+import javax.ejb.Stateless;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author KennediMalheiros
+ */
+@Stateless
+public class BandeiraDao extends GenericDao<Bandeira> implements IRepositoryBandeira {
+
+    public BandeiraDao() {
+        super(Bandeira.class);
+    }
+
+    @Override
+    public Bandeira getBandeiraSigla(String sg) {
+        try {
+
+            Query query = getManager().createQuery("select bd from Bandeira bd where bd.sigla = :sigla");
+            query.setParameter("sigla", sg);
+            return (Bandeira) query.getSingleResult();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+}
