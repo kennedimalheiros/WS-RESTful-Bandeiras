dce9b89911049a5f110874d93cfd3825780ebaf1
kennedimalheiros <kennedimalheiros@gmail.com>
2014-09-06 10:49:50 -0300

    Commit Inicia

---
+++ WSBandeira/src/java/br/com/kpc/bandeira/core/Bandeira.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.kpc.bandeira.core;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author KennediMalheiros
+ */
+@Entity
+@Table(name = "urls")
+@XmlRootElement
+public class Bandeira implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(nullable = false)
+    private String sigla;
+    @Column(nullable = false)
+    private String url;
+    @Column(nullable = false)
+    private String nome;
+
+    public Bandeira() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
